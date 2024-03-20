fac06e0dc02a19df14070055de3ef5dd95e0e3ed
kf_clawlab_dell <35198200+krferrier@users.noreply.github.com>
2024-03-20 17:51:49 -0600

    modified Snakefile and wildcards.py

--- workflow/wildcards.py
+++ workflow/wildcards.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
 def generate_observed_combinations(df, stratify_cols):
-    # Convert integer columns to strings
-    df[stratify_cols] = df[stratify_cols].astype(str)
-    
-    # Group the DataFrame by 'stratify_cols' and aggregate the values
-    observed_combinations = df.groupby(stratify_cols).size().reset_index()
-    
-    # Join the values of each combination with "_"
-    observed_combinations['combination'] = observed_combinations[stratify_cols].apply('_'.join, axis=1)
-    
-    return observed_combinations['combination'].tolist()
-
-pheno = pd.read_csv("pheno.csv")
-stratify_cols = ["sex"]
-generate_observed_combinations(pheno, stratify_cols)
+    if len(stratify_cols) > 0:
+        # Convert integer columns to strings
+        df[stratify_cols] = df[stratify_cols].astype(str)
+        
+        # Group the DataFrame by 'stratify_cols' and aggregate the values
+        observed_combinations = df.groupby(stratify_cols).size().reset_index()
+        
+        # Join the values of each combination with "_"
+        observed_combinations['combination'] = observed_combinations[stratify_cols].apply('_'.join, axis=1)
+        
+        return observed_combinations['combination'].tolist()
+    else:
+        # If no stratify_cols provided, return a list with only "all"
+        return ['all']
